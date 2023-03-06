b0f6f4d932c1db97169e5a77dadbef9f95a0d405
dhanavishnu13 <vishnukvka@gmail.com>
2023-03-06 16:32:00 +0530

    Adding Bulid file

---
+++ client/build/precache-manifest.05b3e4879ae9a866b7310346afe5cb3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1480de47b25b331788b088da5f05a576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86091ebbd47eaa037394",
+    "url": "/static/css/main.64eed7de.chunk.css"
+  },
+  {
+    "revision": "153c25878e19f7181cd4",
+    "url": "/static/js/2.d61b6528.chunk.js"
+  },
+  {
+    "revision": "ba7010c376d06fa8e069a1cbd85fd7eb",
+    "url": "/static/js/2.d61b6528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86091ebbd47eaa037394",
+    "url": "/static/js/main.83a19dae.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.83a19dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "8e9763ff2bd5c0571f6be83d85310d40",
+    "url": "/static/media/memories-Logo.8e9763ff.png"
+  },
+  {
+    "revision": "6c7b5ec0184b26d4b93f36ec813156b3",
+    "url": "/static/media/memories-Text.6c7b5ec0.png"
+  }
+]);
