1f3df80705c5af67d747f8c8b3dab4c51ea97a35
Nivetha-AI <nivethathiyagarajan2003@gmail.com>
2023-03-08 00:36:48 +0530

    Changes in base url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'http://localhost:3000' });
 
 API.interceptors.request.use((req) => {
 
