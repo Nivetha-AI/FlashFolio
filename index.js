b066919920e0c3469dd594930a0f3630366a2924
Nivetha-AI <nivethathiyagarajan2003@gmail.com>
2023-03-08 00:08:37 +0530

    Updated the url

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("*", (req,res)=>(
 ))
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://Nivethathiyagarajan:Nivetha123@cluster0.tq8bagf.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
